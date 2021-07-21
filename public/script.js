f2985754fb1dec883277564f172ddd14a0db29ba
mhdhassan99 <mhassan.work99@gmail.com>
2021-07-21 17:46:19 -0400

    Grammar fixes

---
+++ public/script.js
@@ -0,0 +1,30 @@
+let theme = localStorage.getItem('theme')
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for(var i = 0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode 
+        console.log('clicked', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'white'){
+        document.getElementById('theme-style').href = 'default.css'
+    }
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
